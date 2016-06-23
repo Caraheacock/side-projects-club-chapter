f9455e63307e13c4fb9bdd775ee9b32455aec5f9
Cara Heacock <caraheacock@gmail.com>
2016-06-22 19:24:40 -0500

    Updated spc-common-styles, reduced font size of blog post titles

--- loop.php
+++ loop.php
@@ -1,5 +1,5 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class('puzzle-loop'); ?>>
-    <h2><a href="<?php echo get_the_permalink($post->ID);?>"><?php the_title(); ?></a></h2>
+    <h3><a href="<?php echo get_the_permalink($post->ID);?>"><?php the_title(); ?></a></h3>
 
     <?php if (get_post_type($post) == 'post') : ?>
     <h5><?php the_time('F j, Y'); ?>, by <?php the_author(); ?></h5>
